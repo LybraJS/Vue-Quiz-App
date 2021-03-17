c7bc928371d209db5c31e213f844a4a0a5df4897
SWFTR <mirko.scheschonka@gmail.com>
2021-03-17 06:46:00 +0100

    still have to fix incorret answers cannot be found

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,18 +2,36 @@
     <div>
         <b-jumbotron>
                 <template #lead>
-                Some question here?
+                {{currentQuestion.question}}
                 </template>
 
                 <hr class="my-4">
 
-                <p>
-                List of answers
+                <p v-for='(answer, index) in answers' :key='index'>
+                    {{answer}}
                 </p>
 
                 <b-button variant="primary" href="#">Submit</b-button>
-                <b-button variant="success" href="#">Next</b-button>
+                <b-button @click='next' variant="success" href="#">
+                    Next
+                </b-button>
         </b-jumbotron>
     </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            currentQuestion: Object,
+            next: Function
+        },
+        computed: {
+            answers() {
+               let answers = [...this.currentQuestion.incorrect_answers]
+               return answers.push[this.currentQuestion.correct_answer]
+               
+            }
+        }
+    }
+</script>
              
