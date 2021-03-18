9a00a4bc32cded76e56d9d7cde9af619d1793323
SWFTR <mirko.scheschonka@gmail.com>
2021-03-18 16:36:48 +0100

    included api, fixed bugs and added some styling

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       <b-row>
         <b-col sm='6' offset='3'>
           <QuestionBox
+          v-if='questions.length'
           :currentQuestion='questions[index]'
           :next='next'
           />
